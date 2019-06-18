97d45e52e7251955c020f475276060d950548228
Nick Rosen <rosennm@gmail.com>
2019-06-18 10:10:14 -0700

    change blog date format and start formatting index

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,20 @@
 <template lang="html">
   <div class="main">
-    <div class="main-header">
+    <!-- <div class="main-header">
       <h1>{{siteInfo.sitename}}</h1>
       <p>{{siteInfo.sitedescription}}</p>
+    </div> -->
+    <div class="post" v-for="post in blogPosts" >
+      <h2>{{post.title}}</h2>
+      <h4>{{post.date}}</h4>
+      <n-link :to="post._path">View Post</n-link>
+      {{post}}
     </div>
     
-    <div class="posts-list">
+    <!-- <div class="posts-list">
       <h2 class="posts-list-title">Posts</h2>
       <p class="post-link" v-for="post in blogPosts"><n-link :to="post._path">{{post}}</n-link></p>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -31,4 +37,7 @@ export default {
 </script>
 
 <style scoped>
+.post {
+  border-bottom: 1px solid;
+}
 </style>
